63ee93077e8e0ef6324759d4677897e8bee8d2f1
Aashutosh Soni <aashutosh@synquic.in>
2025-09-01 01:33:54 +0530

    Refactor: changes before mass changes for onboarding.

--- frontend/src/utils/parseDate.ts
+++
@@ -1,5 +0,0 @@
-import { DateTime } from 'luxon';
-
-export default function ParseDate(isoString: string): string {
-    return DateTime.fromISO(isoString, { zone: 'utc' }).toFormat("dd LLLL yyyy, hh:mm a");
-}
